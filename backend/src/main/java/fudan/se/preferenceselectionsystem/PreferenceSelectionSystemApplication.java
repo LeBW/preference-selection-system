b93d0bce7a2d7577c8872912a28677e9b94ab793
LeBW <27091925@qq.com>
2020-04-23 16:27:27 +0800

    init backend project

---
+++ backend/src/main/java/fudan/se/preferenceselectionsystem/PreferenceSelectionSystemApplication.java
@@ -0,0 +1,13 @@
+package fudan.se.preferenceselectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreferenceSelectionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreferenceSelectionSystemApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/fudan/se/preferenceselectionsystem/PreferenceSelectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fudan.se.preferenceselectionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreferenceSelectionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
