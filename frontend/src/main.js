90f9214c90012558ff10534623b90c9afad462bd
YanQianyu <284989344@qq.com>
2020-05-01 15:30:46 +0800

    修改axios base_url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,7 +14,7 @@ process.env.Mock && require('./mock.js')
 var axios = require('axios')
 // Axios挂载到prototype，全局可以使用this.$axios访问
 Vue.prototype.$axios = axios
-axios.defaults.baseURL = '/preference/se/api'
+axios.defaults.baseURL = '/preference/cs/api'
 axios.defaults.withCredentials = true
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
 
